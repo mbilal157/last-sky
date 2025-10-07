b2de18d69600639abbf12a26b57954110f5cb542
Bilal Shahid <rb7910076@gmail.com>
2025-10-07 11:00:11 +0500

    Thumbnails updated

--- app/components/graphic-design/thumbnails.tsx
+++ app/components/graphic-design/thumbnails.tsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from "../ui/lmov-cards";
 
 export function Thumbnails() {
   // Split into 5 rows: 8,8,8,8,9
-  const rowSizes = [8, 8, 8, 8, 9];
+  const rowSizes = [8, 8, 9, 8, 9, 9];
 
   // Slice logos into chunks
   const chunkedThumbnails = [];
@@ -42,6 +42,8 @@ export function Thumbnails() {
 
 export const thumbnails = [
   { image: "/images/portfolio/thumbnails/thumb1.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb47.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb48.jpg" },
   { image: "/images/portfolio/thumbnails/thumb2.jpg" },
   { image: "/images/portfolio/thumbnails/thumb3.jpg" },
   { image: "/images/portfolio/thumbnails/thumb4.jpg" },
@@ -49,6 +51,8 @@ export const thumbnails = [
   { image: "/images/portfolio/thumbnails/thumb6.jpg" },
   { image: "/images/portfolio/thumbnails/thumb7.jpg" },
   { image: "/images/portfolio/thumbnails/thumb8.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb41.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb42.jpg" },
   { image: "/images/portfolio/thumbnails/thumb9.jpg" },
   { image: "/images/portfolio/thumbnails/thumb10.jpg" },
   { image: "/images/portfolio/thumbnails/thumb11.jpg" },
@@ -59,12 +63,16 @@ export const thumbnails = [
   { image: "/images/portfolio/thumbnails/thumb16.jpg" },
   { image: "/images/portfolio/thumbnails/thumb17.jpg" },
   { image: "/images/portfolio/thumbnails/thumb18.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb43.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb44.jpg" },
   { image: "/images/portfolio/thumbnails/thumb19.jpg" },
   { image: "/images/portfolio/thumbnails/thumb20.jpg" },
   { image: "/images/portfolio/thumbnails/thumb21.jpg" },
   { image: "/images/portfolio/thumbnails/thumb22.jpg" },
   { image: "/images/portfolio/thumbnails/thumb23.jpg" },
   { image: "/images/portfolio/thumbnails/thumb24.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb45.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb46.jpg" },
   { image: "/images/portfolio/thumbnails/thumb25.jpg" },
   { image: "/images/portfolio/thumbnails/thumb26.jpg" },
   { image: "/images/portfolio/thumbnails/thumb27.jpg" },
@@ -77,9 +85,11 @@ export const thumbnails = [
   { image: "/images/portfolio/thumbnails/thumb34.jpg" },
   { image: "/images/portfolio/thumbnails/thumb35.jpg" },
   { image: "/images/portfolio/thumbnails/thumb36.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb49.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb50.jpg" },
   { image: "/images/portfolio/thumbnails/thumb37.jpg" },
   { image: "/images/portfolio/thumbnails/thumb38.jpg" },
   { image: "/images/portfolio/thumbnails/thumb39.jpg" },
   { image: "/images/portfolio/thumbnails/thumb40.jpg" },
-  { image: "/images/portfolio/thumbnails/thumb41.jpg" },
+  { image: "/images/portfolio/thumbnails/thumb51.jpg" },
 ];
